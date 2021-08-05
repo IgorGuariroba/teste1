2643db4debe94a96ba3ee3f074e57f117baf7410
IgorGuariroba <1g0r.guari@gmail.com>
2021-08-05 00:20:10 -0300

    Estrutura de um teste automatizado Arrange Act Assert

--- testeAvaliador.php
+++ testeAvaliador.php
@@ -1,5 +1,8 @@
 <?php
-
+/**
+ *  Arrange Act Assert
+ *  GivenWhenthen
+ */
 use Alura\Leilao\Model\Lance;
 use Alura\Leilao\Model\Leilao;
 use Alura\Leilao\Model\Usuario;
@@ -7,7 +10,7 @@ use Alura\Leilao\Service\Avaliador;
 
 require 'vendor/autoload.php';
 
-
+// Organizo o codigo para executar o teste
 $leilao = new Leilao('Fiat 147 0km');
 
 $maria = new Usuario('Maria');
@@ -16,10 +19,18 @@ $joao = new Usuario('João');
 $leilao->recebeLance(new Lance($maria, 2000));
 $leilao->recebeLance(new Lance($joao, 2500));
 
+// Executo o código a ser testado
 $leiloeiro = new Avaliador();
 $leiloeiro->avaliar($leilao);
 
 $maiorValor = $leiloeiro->getMaiorValor();
 
+// Verifico se a saida está correta
+$valorEsperado = 2500;
+
+if($maiorValor != $valorEsperado){
+    echo 'Teste falhou: ' . PHP_EOL;
+    exit;
+}
 
 echo 'Maior valor: ' . $maiorValor . PHP_EOL;
